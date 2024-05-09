6cb300d7bae1c867f93862a4b5b331fcab33cdf0
andreluizborgess <andreluizborges1995@gmail.com>
2024-05-09 13:25:58 -0300

    0905

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Roupas;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        for ($i = 0; $i < 100; $i++) {
+            Roupas::create([
+                'tecido' => 'tecido' . $i,
+                'tamanho' => 'tamanho' . $i,
+                'cor' => 'cor' . $i, 
+                'categoria' => 'categoria' . $i,
+                'fabricacao' => 'fabricacao' . $i,
+                'estacao' => 'estacao' . $i,
+                'descricao'  => 'descricao' . $i
+    
+    
+    
+            ]);
+        }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+      
     }
 }
